651917c85b487af8ba18604b43b44aa5e3747770
Matio004 <mateuszkupis@interia.eu>
2025-03-07 17:53:52 +0100

    bisect + newton, no stop condition

--- base.py
+++ base.py
@@ -13,4 +13,23 @@ def polynomial(x, args):
         y = y*x + i
     return y
 
-print(polynomial(1, [2, 1]))
+def bisect(func, a, b, eps):
+    x0 = (a + b) / 2
+    while abs(a - b) > eps:
+        x0 = (a + b) / 2
+        if func(x0) * func(a) < 0:
+            b = x0
+        elif func(x0) * func(b) < 0:
+            a = x0
+    return x0
+
+def newton(func, derivative, a, b, eps):
+    x_i = a
+    x_i1 = a
+
+    for i in range(10):
+        x_i1, x_i = x_i - func(x_i)/derivative(x_i), x_i1
+    return x_i1
+
+print(bisect(lambda x: polynomial(x, [1.,2.,-2.]), -6., -2., 1e-10))
+print(newton(lambda x: polynomial(x, [1.,2.,-2.]), lambda x: polynomial(x, [2.,2.]), -6., -2., 1e-10))
