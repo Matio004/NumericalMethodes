30ec744af24d72b80529b0e7ec29c852f36e6682
Matio004 <mateuszkupis@interia.eu>
2025-03-08 16:28:10 +0100

    stops

--- base.py
+++ base.py
@@ -13,22 +13,26 @@ def polynomial(x, args):
         y = y*x + i
     return y
 
-def bisect(func, a, b, eps):
-    while abs(a - b) > eps:
+def bisect(func, a, b, eps = None, max_iter = None):
+    steps = 0
+    while (abs(a - b) > eps) if max_iter is None else max_iter > steps:
+        steps += 1
         x0 = (a + b) / 2
         if func(x0) * func(a) < 0:
             b = x0
         elif func(x0) * func(b) < 0:
             a = x0
-    return x0
+    return x0, steps
 
-def newton(func, derivative, a, b, eps):
+def newton(func, derivative, a, b, eps = None, max_iter = None):
     x_i = a
-    x_i1 = a
+    x_i1 = b
 
-    for i in range(10):
+    steps = 0
+    while (abs(x_i1 - x_i) > eps) if max_iter is None else max_iter > steps:
+        steps += 1
         x_i1, x_i = x_i - func(x_i)/derivative(x_i), x_i1
-    return x_i1
+    return x_i1, steps
 
 print(bisect(lambda x: polynomial(x, [1.,2.,-2.]), -6., -2., 1e-10))
-print(newton(lambda x: polynomial(x, [1.,2.,-2.]), lambda x: polynomial(x, [2.,2.]), -6., -2., 1e-10))
+print(newton(lambda x: polynomial(x, [1.,2.,-2.]), lambda x: polynomial(x, [2.,2.]), -6., -2., .0000000001))
