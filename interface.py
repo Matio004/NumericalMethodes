202623252f99b07847c6622677cb967fa4254620
Matio004 <mateuszkupis@interia.eu>
2025-03-11 11:25:49 +0100

    functions

--- interface.py
+++ interface.py
@@ -1,11 +1,13 @@
+from os.path import split
 from math import *
+
 from base import *
 
 print(
 '''Wybierz funkcję:
-0. Wielomian y = 5x^4-6.5x^3+3x^2-6x^2-10x - 22
-1. Trygonometryczna y = sin(x) + cos(x)
-2. Wykładnicza y = e^(x-1) - 2 
+0. Wielomian 2x^5-4x^4+3x^3-6x^2+5x+1
+1. Trygonometryczna
+2. Wykładnicza 
 3. Złożenie funkcji'''
 )
 choice = int(input('Wybór: '))
@@ -13,11 +15,11 @@ match choice:
     case 0:
         func = polynomial(x, [5,-6.5,3,-6,-10, -22])
     case 1:
-        func = sin(x) + cos(x)
+        func = lambda x: sin(x) + cos(x)
     case 2:
-        func = exponential(x,exp(x-1))
+        func = lambda x: exponential(x, 2) - 2
     case 3:
-
+        func = lambda x: sin(polynomial(x, [5, -6.5, 3, -6, -10, -22]))
     case _:
         pass
 epsilon = float(input("Podaj żądaną dokładność: "))
