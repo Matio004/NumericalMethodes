8420ff08ca6d528c8086c886fd86d3f56b16a17b
jkozlowsky <janek1410.jk@gmail.com>
2025-03-11 11:22:00 +0100

    Kolejna aktualizacja interfejsu

--- interface.py
+++ interface.py
@@ -1,24 +1,27 @@
-from os.path import split
 from math import *
-
 from base import *
 
 print(
 '''Wybierz funkcję:
-0. Wielomian 2x^5-4x^4+3x^3-6x^2+5x+1
-1. Trygonometryczna
-2. Wykładnicza 
+0. Wielomian y = 5x^4-6.5x^3+3x^2-6x^2-10x - 22
+1. Trygonometryczna y = sin(x) + cos(x)
+2. Wykładnicza y = e^(x-1) - 2 
 3. Złożenie funkcji'''
 )
 choice = int(input('Wybór: '))
 match choice:
     case 0:
-        func = None
+        func = polynomial(x, [5,-6.5,3,-6,-10, -22])
     case 1:
-        func = sin
+        func = sin(x) + cos(x)
     case 2:
-        func = cos
+        func = exponential(x,exp(x-1))
     case 3:
-        func = tan
+
     case _:
         pass
+epsilon = float(input("Podaj żądaną dokładność: "))
+bisection=bisect(lambda x: func,-1,3, eps=epsilon)
+#TODO wykres
+#TODO przedział
+#TODO rysunek rozw.
