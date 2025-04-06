75d65bb5409a7cc8265f5c5b837c462be28c9fb8
jkozlowsky <janek1410.jk@gmail.com>
2025-04-06 18:07:22 +0200

    Kronecker-Capelli

--- zad2/base.py
+++ zad2/base.py
@@ -1,5 +1,18 @@
 import numpy
 
+def kronecker_capelli(matrix: numpy.ndarray, b: numpy.ndarray):
+    n = matrix.shape[0]
+    coeff_rank = numpy.linalg.matrix_rank(matrix) #macierz współczynników
+    augmented = numpy.hstack((matrix, b.reshape(-1,1))) #macierz rozszerzona [matrix|b]
+    aug_rank = numpy.linalg.matrix_rank(augmented)
+
+    if coeff_rank == aug_rank == n:
+        return 'oznaczony'
+    elif coeff_rank == aug_rank < n:
+        return 'nieoznaczony'
+    else:
+        return 'sprzeczny'
+
 def jordan(matrix: numpy.ndarray, b: numpy.ndarray):
     new_matrix = matrix.copy()
     new_b = b.copy()
@@ -20,10 +33,17 @@ def jordan(matrix: numpy.ndarray, b: numpy.ndarray):
     if (matrix == numpy.identity(matrix.shape[0])).all():
         return b
 
+def solution(matrix: numpy.ndarray, b: numpy.ndarray):
+    status = kronecker_capelli(matrix, b)
+    if status != 'oznaczony':
+        print(f'Układ jest {status}.')
+        return None
+    else:
+        return jordan(matrix, b)
 
-x = jordan(numpy.array([
+x = solution(numpy.array([
     [3, 3, 1],
     [2, 5, 7],
     [-4, -10, -14]
-], dtype=numpy.float64), numpy.array([1, 20, -40], dtype=numpy.float64))
+], dtype=numpy.float64), numpy.array([1, 20, -20], dtype=numpy.float64))
 print(x)
